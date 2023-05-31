653cc4f89f85580d6914e7e94635b2f95e5ef644
ewalter5 <ewalter2055@gmail.com>
2023-05-31 15:27:28 -0400

    new types

--- index.ts
+++ index.ts
@@ -1,3 +1,19 @@
 import * as _ from 'lodash';
 
 console.log('hello world')
+
+type Style = 'bold' | 'italic';
+
+let font: Style;
+
+font = 'bold'
+
+interface Person{
+    first: string;
+    last: string;
+}
+
+const person: Person ={
+    first: 'Eric',
+    last:'Walter'
+}
